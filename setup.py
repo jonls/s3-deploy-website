515827c7f958d89578334ca05d83a59d82fada45
Jon Lund Steffensen <jonlst@gmail.com>
2015-09-17 20:38:47 -0400

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as f:
 
 setup(
     name='s3-deploy-website',
-    version='0.0.1',
+    version='0.1.0',
     license='MIT',
     author='Jon Lund Steffensen',
     author_email='jonlst@gmail.com',
